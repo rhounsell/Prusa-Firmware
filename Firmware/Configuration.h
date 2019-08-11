521429ecda0d608880e8a0419e02b851baf9dd57
rhounsell <53660397+rhounsell@users.noreply.github.com>
2019-08-11 17:16:57 -0400

    Fixes for FlashAir card, and VS Code configuration

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -7,7 +7,7 @@
 #define STR(x) STR_HELPER(x)
 
 // Firmware version
-#define FW_VERSION "3.7.2"
+#define FW_VERSION "3.7.2-RDH"
 #define FW_COMMIT_NR   2363
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
